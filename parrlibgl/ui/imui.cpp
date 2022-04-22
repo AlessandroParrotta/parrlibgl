d0af366de5d33e2d615f1486b073096c68637ecc
Alessandro Parrotta <alessandro.parrott@gmail.com>
2022-04-22 15:58:35 +0200

    imui fix

--- parrlibgl/ui/imui.cpp
+++ parrlibgl/ui/imui.cpp
@@ -1021,7 +1021,7 @@ namespace prb {
 			bool rem = true;
 			while (rem) {
 				rem = false;
-				for (auto& c : pingContainers) if (!c.second) { containers.erase(c.first); pingContainers.erase(c.first); posContainers.erase(c.first); rem = true; break; }
+				for (auto& c : pingContainers) if (!c.second) { containers.erase(c.first); posContainers.erase(c.first); pingContainers.erase(c.first); rem = true; break; }
 			}
 		
 
